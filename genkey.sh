4229534eb5aea3f62493618eb00300596146c026
Kevin Sillerud <kevin.sillerud@gmail.com>
2019-01-10 15:00:10 +0100

    Generate certificate on the fly for MQ

---
+++ genkey.sh
@@ -0,0 +1,6 @@
+keytool -genkey -noprompt \
+ -alias alias1 \
+ -dname "CN=sdpkanal-nais, OU=Unknown, O=NAV, L=Oslo, S=Oslo, C=NO" \
+ -keystore keystore.p12 \
+ -storepass changeit \
+ -keypass changeit
