fcba55e3b65d74005e3a0e53c96ff89658899163
Kevin Sillerud <kevin.sillerud@gmail.com>
2019-01-29 13:22:47 +0100

    Simplify logic for getting conversation id from receipts and add truststore location to docker file

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM navikt/java:11
 
 ENV NO_NAV_SDPKANAL_DOKUMENT_PATH_PREFIX='/q1/dokumentdistribusjon/documentFileshare/'
 ENV NO_NAV_SDPKANAL_KEYSTORE=/var/run/secrets/nais.io/vault/virksomhet.jks.b64
+ENV NO_NAV_SDPKANAL_TRUSTSTORE=/var/run/secrets/nais.io/vault/truststore.jks.b64
 ENV NO_NAV_SDPKANAL_CREDENTIALSPATH=/var/run/secrets/nais.io/vault/credentials.json
 ENV NO_NAV_SDPKANAL_SFTP_KEY_PATH=/var/run/secrets/nais.io/vault/sftp_key
 ENV NO_NAV_SDPKANAL_SFTP_KNOWN_HOSTS=/var/run/secrets/nais.io/vault/known_hosts
--- src/main/kotlin/no/nav/kanal/ebms/EbmsReceiptExtractor.kt
+++ src/main/kotlin/no/nav/kanal/ebms/EbmsReceiptExtractor.kt
@@ -8,15 +8,10 @@ import org.slf4j.LoggerFactory
 import org.springframework.ws.WebServiceMessage
 import org.springframework.ws.client.core.WebServiceMessageExtractor
 import org.springframework.ws.soap.SoapMessage
-import org.w3c.dom.Node
-import org.w3c.dom.NodeList
 import toByteArray
 import java.io.ByteArrayOutputStream
-import java.nio.file.Files
-import java.nio.file.Paths
 import javax.xml.transform.OutputKeys
 import javax.xml.transform.TransformerFactory
-import javax.xml.transform.dom.DOMSource
 import javax.xml.transform.stream.StreamResult
 
 private const val NO_MESSAGE_AVAILABLE_FROM_MPC_ERROR_CODE = "EBMS:0006"
@@ -31,21 +26,12 @@ class EbmsReceiptExtractor(val legalArchiveLogger:  LegalArchiveLogger) : EbmsCo
                 null
             } else {
                 val soapBody = (message as SoapMessage).soapBody
-                val node = (soapBody.payloadSource as DOMSource).node
-                /*
-                This might be a bit hacky way of extracting the conversationId, however it does not require a strict
-                XPath or re-parsing of the document, so its a simple but hacky way of extracting it
-                 */
-                val conversationId = node
-                        ?.childNodes?.find("StandardBusinessDocumentHeader")
-                        ?.childNodes?.find("BusinessScope")
-                        ?.childNodes?.find("Scope")
-                        ?.childNodes?.find("InstanceIdentifier")
-                        ?.firstChild?.nodeValue
+                val conversationId = ebmsContext.sbd.underlyingDoc.standardBusinessDocumentHeader
+                        .businessScope.scopes.first().instanceIdentifier
+
                 runBlocking {
                     legalArchiveLogger.archiveDocumentLogOnException(conversationId!!, "NAV", "DIFI meldingsformidler", message.toByteArray(this), EVENT_TYPE)
                 }
-                Files.newOutputStream(Paths.get("receipt_$conversationId.bin")).use { message.writeTo(it) }
                 EbmsReceipt(
                         sbdBytes = ByteArrayOutputStream().use {
                             tf.newTransformer()
@@ -61,13 +47,5 @@ class EbmsReceiptExtractor(val legalArchiveLogger:  LegalArchiveLogger) : EbmsCo
                         references = ebmsContext.incomingReferences.values
                 )
             }
-
-    private fun NodeList.find(name: String): Node? {
-        for (i in 0.until(length)) {
-            if (item(i)?.localName == name)
-                return item(i)
-        }
-        return null
-    }
 }
 
