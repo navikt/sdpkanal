3a8621e5224037536efe814430b94c663d91e127
Kevin Sillerud <kevin.sillerud@gmail.com>
2019-02-18 14:55:08 +0100

    Lower to max 4 pods

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.jvm.tasks.Jar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "no.nav.sdp-kanal"
-version = "1.3.4"
+version = "1.3.5"
 
 description = """Application for routing Secure Digital Post to DIFI"s message dispatcher"""
 
